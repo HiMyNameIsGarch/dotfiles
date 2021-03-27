5b2f99ae6238d1e5e91380744241c900c3ac74d2
HiMyNameIsGarch <gabrielhanu56@yahoo.com>
2021-03-27 15:23:18 +0200

    add zsh and aliasrc

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Put wallpaper
-feh --bg-scale ~/Wallpapers/space.jpg
-
-# Output sound to speakers
-amixer -c 1 sset "Auto-Mute Mode" Disabled
-
-# Start compozitor picom
-picom --experimental-backends
+/home/himynameisgarch/.config/qtile/autostart.sh
