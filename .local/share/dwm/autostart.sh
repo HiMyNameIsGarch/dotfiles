234c69f27eca1f07722b73db9a1640ceed743fe8
HiMyNameIsGarch <gabrielhanu56@yahoo.com>
2021-04-03 16:12:28 +0300

    add local scripts

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Put wallpaper
+feh --bg-scale /home/himynameisgarch/Media/Wallpapers/Doom/Doom-Eternal_Maurader_Wallpaper_1920x1080-01.jpg
+
+# Start dwmblocks
+dwmblocks >/dev/null & 
+
+# Start compositor picom
+picom --experimental-backends >/dev/null &
+
+# Start redshift
+redshift >/dev/null &
+
+# Output sound to speaker 
+soundCard="HDA-Intel - HDA Intel PCH"
+cardNum="$(cat /proc/asound/cards | grep $soundCard | awk '{print $1}')"
+amixer -c $cardNum sset "Auto-Mute Mode" 'Speaker Only' </dev/null
+
+
