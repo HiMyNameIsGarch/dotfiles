02441b1b46b334dad5d35927baef8cc39a53b5eb
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-06-29 19:18:33 +0300

    add uptime logs before shutdown/reboot

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
 # Put wallpaper
-feh --bg-scale --no-fehbg /home/himynameisgarch/Media/Wallpapers/Doom/Doom-Eternal_Maurader_Wallpaper_1920x1080-01.jpg &
+feh --bg-scale --no-fehbg /home/himynameisgarch/Media/Wallpapers/coffeeBreak.png
 
 # Start dwmblocks
 dwmblocks >/dev/null & 
 
 # Start compositor picom
-picom --experimental-backends >/dev/null &
+# picom --experimental-backends >/dev/null &
 
 # Start redshift
-killall redshift
-redshift >/dev/null &
+killall redshift ; redshift >/dev/null &
 
 # Start dunst 
 dunst & 
 
 # Output sound to speaker 
 soundCard="HDA-Intel - HDA Intel PCH"
-cardNum="$(cat /proc/asound/cards | grep "$soundCard" | awk '{print $1}')"
+cardNum="$(grep "$soundCard" "/proc/asound/cards" | awk '{print $1}')"
 amixer -c $cardNum sset "Auto-Mute Mode" 'Speaker Only' </dev/null
 
 
