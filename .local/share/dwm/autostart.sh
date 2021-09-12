e0d0915d84c0002cb7cca1066ce295eac370057b
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-09-12 13:05:19 +0300

    minor changes: aliases, functions, confs and more

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Put wallpaper
-feh --bg-scale --no-fehbg /home/himynameisgarch/Media/Wallpapers/Simple/Arch.png
+feh --bg-scale --no-fehbg --recursive --randomize /home/himynameisgarch/Media/Wallpapers/
 
 # Start dwmblocks
 dwmblocks >/dev/null & 
@@ -16,7 +16,7 @@ xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 # picom --experimental-backends >/dev/null &
 
 # Start redshift
-killall redshift ; redshift >/dev/null &
+redshift >/dev/null &
 
 # Start dunst 
 dunst & 
@@ -25,5 +25,3 @@ dunst &
 soundCard="HDA-Intel - HDA Intel PCH"
 cardNum="$(grep "$soundCard" "/proc/asound/cards" | awk '{print $1}')"
 amixer -c "$cardNum" sset "Auto-Mute Mode" 'Speaker Only' </dev/null
-
-
