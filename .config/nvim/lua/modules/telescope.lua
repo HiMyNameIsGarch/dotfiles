5e45ac1e0591d80c40483b62b9f8e7a2b64713ef
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-06-13 17:44:58 +0300

    modify telescope maps

--- .config/nvim/lua/modules/telescope.lua
+++ .config/nvim/lua/modules/telescope.lua
@@ -23,33 +23,21 @@ telescope.setup {
 }
 telescope.load_extension('fzy_native')
 
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
-
 -- Maps
-keymap.set('n', '<C-_>', function ()
+local map = function(lhs, rhs)
+    vim.keymap.set("n", lhs, rhs, { noremap = true, silent = true })
+end
+
+map('<C-_>', function ()
     require('telescope.builtin').current_buffer_fuzzy_find({sorting_strategy="ascending", layout_config={prompt_position="top"}})
-end , opts)
-
-keymap.set('n', '<leader>ff', function ()
-    require('telescope.builtin').find_files() end , opts)
-
-keymap.set('n', '<leader>ps', function ()
+end)
+map('<leader>ff', require('telescope.builtin').find_files)
+map('<leader>ps', function ()
     require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})
-end , opts)
-
-keymap.set('n', '<leader>lg', function ()
-    require('telescope.builtin').live_grep() end , opts)
-
-keymap.set('n', '<leader>gf', function ()
-    require('telescope.builtin').git_files() end , opts)
-
-keymap.set('n', '<leader>df', function ()
+end)
+map('<leader>lg', require('telescope.builtin').live_grep)
+map('<leader>gf', require('telescope.builtin').git_files)
+map('<leader>df', function ()
     require("telescope.builtin").find_files({ prompt_title = "< DotFiles >", cwd = "~/Dev/dotfiles", hidden = true, })
-end , opts)
-
-keymap.set('n', '<leader>bf', function ()
-    require('telescope.builtin').buffers() end , opts)
-
-keymap.set('n', '<leader>fb', function ()
-    require('telescope.builtin').file_browser() end , opts)
+end)
+map('<leader>bf', require('telescope.builtin').buffers)
