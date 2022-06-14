888d54cb89a3b74225bf076ef08dd3e6dfef95f2
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-06-14 13:50:52 +0300

    dynamic pid when attach to server + dap ui config

--- .config/nvim/lua/modules/debugger.lua
+++ .config/nvim/lua/modules/debugger.lua
@@ -18,11 +18,16 @@ dap.adapters.coreclr = {
   args = { '--interpreter=vscode' }
 }
 
-function GetDotnetExec()
-    local cwd = vim.fn.getcwd()
-    local relPath = vim.split(cwd, "/", true)
-    local proj = relPath[#relPath] or "Changeme"
-    return cwd .. '/bin/Debug/net6.0/' .. proj .. '.dll'
+function GetProjName()
+    local relPath = vim.split(vim.fn.getcwd(), "/", true)
+    return relPath[#relPath] or "NotFound"
+end
+
+function GetPID()
+    local cmd = { "pidof", GetProjName() }
+    local pid = vim.fn.system(cmd)
+    -- make sure to return the first one found
+    return vim.split(pid, " ", true)[1]
 end
 
 dap.configurations.cs = {
@@ -30,14 +35,17 @@ dap.configurations.cs = {
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
-    program = GetDotnetExec
+    args = { "status" },
+    program = function()
+        return vim.fn.getcwd() .. '/bin/Debug/net6.0/' .. GetProjName() .. '.dll'
+    end
   },
   {
       type = "coreclr",
       name = "attach - coreclr",
       request = "attach",
       processId = function()
-          local pid = require('dap.utils').pick_process()
+          local pid = GetPID()
           vim.fn.setenv('NETCOREDBG_ATTACH_PID', pid)
           return pid
       end,
@@ -45,7 +53,24 @@ dap.configurations.cs = {
 }
 
 -- DAP-UI
-require("dapui").setup()
+require("dapui").setup({
+    sidebar = {
+        -- You can change the order of elements in the sidebar
+        elements = {
+            -- Provide as ID strings or tables with "id" and "size" keys
+            { id = "scopes", size = 0.33 }, -- Can be float or integer > 1 },
+            { id = "watches", size = 0.34 },
+            { id = "breakpoints", size = 0.33 },
+        },
+        size = 40,
+        position = "left", -- Can be "left", "right", "top", "bottom"
+    },
+    tray = {
+        elements = { "repl" },
+        size = 10,
+        position = "bottom", -- Can be "left", "right", "top", "bottom"
+    },
+})
 local dapui = require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
