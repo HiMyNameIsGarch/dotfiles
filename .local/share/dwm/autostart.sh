018173aa9384cc333c4d0c0b7d49d719b6832725
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-09-25 18:20:02 +0300

    audio is somehow down, may God be with this config

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -4,10 +4,10 @@
 feh --bg-scale --no-fehbg --recursive --randomize /home/himynameisgarch/Media/Wallpapers/
 
 # Start dwmblocks
-dwmblocks >/dev/null & 
+dwmblocks >/dev/null &
 
 # Hide mouse when not needed
-unclutter --jitter 50 --ignore-scrolling & 
+unclutter --jitter 50 --ignore-scrolling &
 
 # Map capslock to esc key
 xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
@@ -18,10 +18,13 @@ picom >/dev/null &
 # Start redshift
 redshift >/dev/null &
 
-# Start dunst 
-dunst & 
+# Start dunst
+dunst &
 
-# Output sound to speaker 
+# Pulse audio
+pulseaudio -D
+
+# Output sound to speaker
 soundCard="HDA-Intel - HDA Intel PCH"
 cardNum="$(awk "/$soundCard/ {print \$1}" /proc/asound/cards)"
 amixer -c "$cardNum" sset "Auto-Mute Mode" 'Speaker Only' </dev/null
