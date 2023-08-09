c19eb76cfb0322d4f9d887aa3cf29fe19055701f
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2023-08-09 06:05:30 +0300

    volar over vuels, change my mind

--- .config/nvim/lua/modules/lsp.lua
+++ .config/nvim/lua/modules/lsp.lua
@@ -20,12 +20,12 @@ local function config(_config)
 end
 
 local lspconfig = require("lspconfig")
+
 lspconfig.clangd.setup(config())
 -- lspconfig.ccls.setup(config( {
 --     single_file_support= true
 -- }))
 lspconfig.tsserver.setup(config())
-lspconfig.vuels.setup(config())
 lspconfig.vimls.setup(config())
 lspconfig.bashls.setup(config())
 lspconfig.pyright.setup(config())
@@ -33,6 +33,34 @@ lspconfig.dartls.setup(config())
 lspconfig.cssls.setup(config())
 lspconfig.tailwindcss.setup(config())
 lspconfig.html.setup(config())
+
+-- Vuejs
+local util = require 'lspconfig.util'
+local function get_typescript_server_path(root_dir)
+
+  local global_ts = '/home/garch/.local/share/npm/lib/node_modules/typescript/lib'
+  -- Alternative location if installed as root:
+  -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+  local found_ts = ''
+  local function check_dir(path)
+    found_ts =  util.path.join(path, 'node_modules', 'typescript', 'lib')
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+lspconfig.volar.setup(config({
+    filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
+}))
 
 -- Lua
 lspconfig.lua_ls.setup(config({
