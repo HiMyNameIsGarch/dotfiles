280a7657fdc5f273bd3ba3dbb04c22f15e0d1c68
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2023-09-30 00:41:28 +0300

    disable picom and end execution of scripts

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -13,10 +13,10 @@ unclutter --jitter 50 --ignore-scrolling &
 xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 
 # Start compositor picom
-picom >/dev/null &
+# picom >/dev/null &
 
 # Start redshift
-redshift >/dev/null &
+# redshift >/dev/null &
 
 # Start dunst
 dunst &
@@ -35,7 +35,7 @@ qutebrowser &
 
 sleep 1s
 
-sys_sound_play boot_welcome
+sys_sound_play boot_welcome &
 
 xinput --set-prop 8 'libinput Accel Speed' -0.4
 
