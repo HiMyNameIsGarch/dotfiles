818990ef91276ccee4527d76e0a529b76e4fa0c7
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2024-12-04 19:37:49 +0200

    execute sql commands in vim from visual mode, womp

--- .config/nvim/lua/modules/sqlpls.lua
+++ .config/nvim/lua/modules/sqlpls.lua
@@ -2,21 +2,27 @@ local fo = require("file_operation")
 
 local path_to_buf = "/tmp/sqlisfun"
 
-local function funsql(buf)
-    local current_sql_file = vim.fn.expand('%:p')
+local function start_sql_process(sql_file, buffer)
 
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+    vim.api.nvim_buf_set_lines(buffer, 0, -1, false, {})
 
-    vim.fn.jobstart({ "sqlcmd", "-S", "localhost", "-U", "sa", "-P", "Muielarga69!", "-d", "passman", "-h-1", "-W", "-i", current_sql_file, "-C" }, {
+    vim.fn.jobstart({ "sqlcmd", "-S", "localhost", "-U", "sa", "-P", "Muielarga69!", "-d", "passman", "-h-1", "-W", "-i", sql_file, "-C" }, {
         stdout_buffered = true,
         stderr_buffered = true,
         on_stderr = function (_, data)
-            fo.WriteToBuf(path_to_buf, buf, data)
+            fo.WriteToBuf(path_to_buf,buffer, data)
         end,
         on_stdout = function (_, data)
-            fo.WriteToBuf(path_to_buf, buf, data)
+            fo.WriteToBuf(path_to_buf,buffer, data)
         end
         })
+end
+
+local function funsql(buf)
+    local current_sql_file = vim.fn.expand('%:p')
+
+    start_sql_process(current_sql_file, buf)
+
 end
 
 -- Function to write selected lines to a file
@@ -38,6 +44,12 @@ function _G.write_selection_to_file()
             file:write(line .. "\n")
         end
         file:close()
+        local fbuf = fo.Read_buf_from_file(path_to_buf)
+
+        if fbuf and vim.fn.bufexists(fbuf) == 1 then
+            start_sql_process(file_path, fbuf)
+        end
+
     else
         print("Failed to open file for writing. File path: " .. file_path)
     end
@@ -75,4 +87,3 @@ vim.g.db_ui_save_location = "~/.local/share/db_queries"
 vim.keymap.set('n', '<leader>db', ":DBUIToggle<CR>",            { silent = true, noremap = true })
 vim.keymap.set('n', '<leader>dl', ":DBUIHideNotifications<CR>", { silent = true, noremap = true })
 vim.keymap.set('n', '<leader>di', ":DBUILastQueryInfo<CR>",     { noremap = true })
-
